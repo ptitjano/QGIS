d99fef981dcbc1b0d2c314af89a515d5e20b957c
Simon Lopez <s.lopez@brgm.fr>
2024-09-02 17:03:15 +0200

    Toggle layer edition from elevation profile layer tree

--- src/app/elevation/qgselevationprofilewidget.cpp
+++ src/app/elevation/qgselevationprofilewidget.cpp
@@ -1130,6 +1130,16 @@ void QgsAppElevationProfileLayerTreeView::contextMenuEvent( QContextMenuEvent *e
   {
     QMenu *menu = new QMenu();
 
+    QAction *toggleEditingAction = new QAction( tr( "Toggle Editing" ), menu );
+    toggleEditingAction->setIcon( QgsApplication::getThemePixmap( QStringLiteral( "/mActionToggleEditing.svg" ) ) );
+    connect( toggleEditingAction, &QAction::triggered, this, [layer]
+    {
+      QgisApp::instance()->toggleEditing( layer );
+    } );
+    menu->addAction( toggleEditingAction );
+
+    menu->addSeparator();
+
     QAction *propertiesAction = new QAction( tr( "Properties…" ), menu );
     connect( propertiesAction, &QAction::triggered, this, [layer]
     {
