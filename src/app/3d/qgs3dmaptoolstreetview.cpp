329ba6bf702623de2559aeca1ed20b108663008e
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2025-06-18 15:10:25 +0200

    fixup! when mouse lateral move, invert camera direction move

--- src/app/3d/qgs3dmaptoolstreetview.cpp
+++ src/app/3d/qgs3dmaptoolstreetview.cpp
@@ -322,7 +322,7 @@ void Qgs3DMapToolStreetView::mouseMoveEvent( QMouseEvent *event )
           else
             evPos *= 0.5;
 
-          mCanvas->cameraController()->rotateCamera( evPos.y(), -evPos.x() );
+          mCanvas->cameraController()->rotateCamera( -evPos.y(), -evPos.x() );
 
           if ( mIsOptimal )
           {
