449a01ba99b33592a31883620ca28a671a6390a5
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2025-12-22 14:19:44 +0100

    doc(createprimitivedialog): add doc

--- src/app/3d/qgs3dcreateprimitivedialog.h
+++ src/app/3d/qgs3dcreateprimitivedialog.h
@@ -7,11 +7,20 @@
 
 #include <QDialog>
 
+/**
+ * Base class to primitive creation dialog
+ * \since QGIS 3.44
+ */
 class Qgs3DCreatePrimitiveDialog : public QDialog, protected Ui::CreatePrimitiveDialog
 {
     Q_OBJECT
 
   public:
+    /**
+     * Default constructor
+     * \param type primitive name
+     * \param f window flag
+     */
     Qgs3DCreatePrimitiveDialog( const QString &type, Qt::WindowFlags f = ( Qt::WindowFlags() | Qt::Tool ) );
 
     /**
@@ -28,20 +37,47 @@ class Qgs3DCreatePrimitiveDialog : public QDialog, protected Ui::CreatePrimitive
       YZ    //!< Only on YZ plane
     };
 
+    /**
+     * Set 3 translation coordinates
+     * \param point new translation
+     */
     void setTranslation( const QgsPoint &point );
+
+    /**
+     * Set rotation values in degrees
+     * \param rx x axis rotation
+     * \param ry y axis rotation
+     * \param rz z axis rotation
+     */
     void setRotation( double rx, double ry, double rz );
+
+    /**
+     * Set 3 scale values
+     * \param point new scales
+     */
     void setScale( const QgsPoint &point );
 
+    /// \return X translation value
     double transX() const { return spinTX->value(); }
+    /// \return Y translation value
     double transY() const { return spinTY->value(); }
+    /// \return Z translation value
     double transZ() const { return spinTZ->value(); }
 
+    /// \return X axis rotation value
     double rotX() const { return spinRX->value(); }
+
+    /// \return Y axis rotation value
     double rotY() const { return spinRY->value(); }
+
+    /// \return Z axis rotation value
     double rotZ() const { return spinRZ->value(); }
 
+    /// \return X axis scale value
     double scaleX() const { return spinSX->value(); }
+    /// \return X axis scale value
     double scaleY() const { return spinSY->value(); }
+    /// \return X axis scale value
     double scaleZ() const { return spinSZ->value(); }
 
     /// Focus on create button
@@ -49,7 +85,7 @@ class Qgs3DCreatePrimitiveDialog : public QDialog, protected Ui::CreatePrimitive
     /// Release focus on create button
     void unfocusCreateButton();
 
-    /// \return the number of paramter the user will have to set
+    /// \return the number of parameter the user will have to set
     virtual int paramNumber() const = 0;
 
     /**
@@ -58,16 +94,30 @@ class Qgs3DCreatePrimitiveDialog : public QDialog, protected Ui::CreatePrimitive
      */
     virtual ConstrainedAxis constrainedAxisForParam( int idx ) = 0;
 
+    /**
+     * Set new \a value for specified parameter
+     * \param idx parameter number
+     * \param value new value
+     */
     virtual void setParam( int idx, double value ) = 0;
+
+    /**
+     * \param idx parameter number
+     * \return value for specified parameter
+     */
     virtual double getParam( int idx ) const = 0;
+
+    /// Reset dialog values
     virtual void resetData();
 
   signals:
+    /// Emitted anytime a value is changed
     void valueChanged();
-    /// Emited when create button is clicked
+    /// Emitted when create button is clicked
     void createClicked( bool );
 
   private:
+    /// Primitive type name
     QString mType;
 };
 
